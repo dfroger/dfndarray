cd2b9f86ea5ad648776849fbe29376fc3d721b4c
David Froger <david.froger@inria.fr>
2014-05-28 11:29:10 +0200

    Add home page reference.

--- Makefile
+++ Makefile
@@ -14,6 +14,8 @@
 #You should have received a copy of the GNU General Public License along with
 #dfndarray.  If not, see <http://www.gnu.org/licenses/>.
 
+#Home page: https://github.com/dfroger/dfndarray
+
 default: test.x example.x
 
 test.x: test.o dfndarray.o dfndarray.hxx dfndarray.txx
--- example.cxx
+++ example.cxx
@@ -14,6 +14,8 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
 dfndarray.  If not, see <http://www.gnu.org/licenses/>.
+
+Home page: https://github.com/dfroger/dfndarray
 */
 
 #include "dfndarray.hxx"
