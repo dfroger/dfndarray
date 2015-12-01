91dc11480023cead36a39203291ee55796a64bdd
David Froger <david.froger@inria.fr>
2015-12-01 07:59:47 -0800

    Fixing linking on OSX

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,5 @@
+# TODO: debug version -O0 -g -DDFNDARRAY_RUNTIME_CHECK
+
 if [ "$(uname)" == "Darwin" ]
 then
     export MACOSX_DEPLOYMENT_TARGET=10.7
@@ -13,15 +15,18 @@ then
     $PYTHON ../waf configure \
         --prefix=$PREFIX \
         --python-bindings \
-        --cflags='-mmacosx-version-min=10.7 -arch x86_64' \
-        --cxxflags='-stdlib=libc++ -mmacosx-version-min=10.7 -arch x86_64' \
-        --ldflags='-stdlib=libc++ -mmacosx-version-min=10.7 -arch x86_64' \
+        --cflags='-O3 -DNDEBUG -mmacosx-version-min=10.7 -arch x86_64' \
+        --cxxflags='-O3 -DNDEBUG -stdlib=libc++ -mmacosx-version-min=10.7 -arch x86_64' \
+        --ldflags="-L$PREFIX/lib -O3 -DNDEBUG -stdlib=libc++ -mmacosx-version-min=10.7 -arch x86_64" \
         --remove-isysroot
 else
     cd dfndarray
     $PYTHON ../waf configure \
         --prefix=$PREFIX \
-        --python-bindings
+        --python-bindings \
+        --cflags='-O3 -DNDEBUG' \
+        --cxxflags='-O3 -DNDEBUG' \
+        --ldflags='-O3 -DNDEBUG'
 fi
 
 $PYTHON ../waf build -v
